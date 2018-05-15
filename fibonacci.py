39bf23a419ef440f61b467b8ce84d2357f94dc0e
BrandonSersion <SersionSoftware@protonmail.com>
2018-05-15 17:14:40 -0400

    Use generator on without_recursion

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,7 @@
 """Fibonacci sequence - with and without recursion."""
 
 def with_recursion(num1, num2, limit=1000000):
-    print(num1)
+    print(num1, end=' ')
     num1, num2 = num2, num1 + num2
 
     if num1 < limit:
@@ -13,16 +13,16 @@ def without_recursion(num1, num2, limit=1000000):
         return a, b
 
     while num1 < limit:
-        print(num1)
-        nums = fibonacci(num1, num2)
-        num1, num2 = nums[0], nums[1]
+        yield num1
+        num1, num2 = fibonacci(num1, num2)
 
 
 def main():
     print('With recursion:')
     with_recursion(0, 1, 200)
+    print()
     print('Without recursion:')
-    without_recursion(0, 1, 200)
+    print(*without_recursion(0, 1, 200))
 
 if __name__ == "__main__":
     main()
